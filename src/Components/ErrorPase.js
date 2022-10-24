e8ea65e5eca1865cd614dbde3d06c3efc8e156c2
Naimcoder <isteiaknaim12@gmail.com>
2022-10-24 22:39:16 +0600

    added login page

--- src/Components/ErrorPase.js
+++ src/Components/ErrorPase.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPase = () => {
+    const error = useRouteError();
+  console.error(error);
     return (
  <div>
  <section className="flex items-center h-full p-16bg-gray-900 dark:text-gray-100">
@@ -10,9 +12,10 @@ const ErrorPase = () => {
 			<h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
 				<span className="sr-only">Error</span>404
 			</h2>
-			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-			<p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-			<Link rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
+			<p className="text-2xl font-semibold md:text-3xl">Oops!.</p>
+			<p className="mt-4 mb-8 dark:text-gray-400">Sorry, an unexpected error has occurred..</p>
+            <p>{error.statusText || error.message}</p>
+			<Link rel="noopener noreferrer" to="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
 		</div>
 	</div>
     </section>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h2>this is login</h2>
+           <div className='lg:w-4/12 lg:p-10 sm:mb-6 container lg:my-20 rounded  mx-auto  bg-red-300 p-5'>
+           <h3 className="lg:text-center  mb-4 lg:text3xl font-bold sm:text-center sm:mb-6 sm:text-2xl">
+                  Sign In
+                </h3>
+                <form>
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                    htmlFor="email"
+                     className="text-2xl inline-block mb-1 font-medium"
+                    >
+                     E-mail
+                    </label>
+                    <input
+                      placeholder="Your Email Address"
+                      required
+                      type="text"
+                      className="text-xl flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="email"
+                      name="email"
+                    />
+                  </div>
+                   <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="password"
+                      className="text-2xl inline-block mb-1 font-medium"
+                    >
+                     Password
+                    </label>
+                    <input
+                      placeholder="Enter Your Password"
+                      required
+                      type="password"
+                      className="text-xl flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="password"
+                      name="password"
+                    />
+                  </div>
+                  <div className="mt-4 mb-2 sm:mb-4">
+                    <button
+                      type="submit"
+                      className="text-xl inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide bg-black text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                      Login
+                    </button>
+                     <div className='text-2xl py-3 flex font-bold justify-between '>
+                       
+                        <p className="text-xl  text-gray-600 sm:text-sm">
+                          New User In Webside ? <Link className=' underline  text-cyan-500' to='/register'>Please Register</Link>
+                         </p>
+                   <p className='text-xl underline text-blue-600 underline-offset-1'>Forget Password</p>
+                     </div>
+                  </div>
+                 
+                  
+                </form>
+        </div>
+    );
         </div>
     );
 };
