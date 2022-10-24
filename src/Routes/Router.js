ce5eb54505a26cab7ac1951e0f0e3ad2ec6e6984
Naimcoder <isteiaknaim12@gmail.com>
2022-10-24 21:53:15 +0600

    added page login register

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,4 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../Components/Blog/Blog";
+import Courses from "../Components/Courses/Courses";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 import Root from "../Layout/Root";
 
 export const router = createBrowserRouter([
@@ -6,9 +11,27 @@ export const router = createBrowserRouter([
     path:'/',
     element:<Root></Root>,
     children:[
-        {},
-        {},
-        {}
+        {path:'/',
+        element:<Home></Home>,
+        },
+        {path:'/home',
+        element:<Home></Home>
+        },
+        {path:'/courses',
+        element:<Courses></Courses>
+        },
+        {
+         path:"/login",
+         element:<Login></Login>
+        },
+        {
+            path:"/register",
+            element:<Register></Register>
+        },
+        {
+            path:'/blog',
+            element:<Blog></Blog>
+        }
     ]
 }
 ])
